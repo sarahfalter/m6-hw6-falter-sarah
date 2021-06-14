dacaf161b6ec3209cba67ad48fc885a80fb10717
sarahfalter <sarah.falter@ufl.edu>
2021-06-13 23:06:22 -0400

    Add javascript and styling

--- main.js
+++ main.js
@@ -1,4 +1,44 @@
-document.querySelector("div").addEventListener("mouseover", function () {
 
-    document.querySelector("div").style.backgroundColor = "red";
-})
+var background = document.getElementsByClassName("bg-box");
+
+document.background.addEventListener("mouseenter", function () {
+   document.getElementsByClassName("bg-box").style.backgroundColor = "red";
+}, false
+);
+
+
+
+
+
+
+var newPara = document.createElement("p");
+
+document.getElementById('box1').addEventListener("click"
+,function() {
+    newPara.innerHTML = "Oooh - so close, but no cigar";
+    document.getElementById('new1').appendChild(newPara);
+}, false
+);
+
+document.getElementById('box2').addEventListener("click"
+, function () {
+    newPara.innerHTML = "DING DING DING - We have a winner";
+    document.getElementById('new2').appendChild(newPara);
+}, false
+);
+
+document.getElementById('box3').addEventListener("click"
+, function () {
+    newPara.innerHTML = "Oops, butter luck next time";
+    document.getElementById('new3').appendChild(newPara);
+}, false
+);
+
+var btn = document.createElement("button");
+
+document.getElementById('main').addEventListener("click"
+, function() {
+    btn.innerHTML = "<a href='index.html'>Start Over</a>";
+    document.getElementById('main').appendChild(btn);
+}, false 
+); 
